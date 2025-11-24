c414e99c791cce638396ba1d5f741f34fd9a58fb
Sonila <matosonila94@gmail.com>
2025-11-24 23:44:23 +0100

    adding missing files (#2)

---
+++ library/tasks.py
@@ -0,0 +1,31 @@
+from celery import shared_task
+from .models import Loan
+from django.core.mail import send_mail
+from django.conf import settings
+
+@shared_task
+def send_loan_notification(loan_id):
+    try:
+        loan = Loan.objects.get(id=loan_id)
+        member_email = loan.member.user.email
+        book_title = loan.book.title
+        send_mail(
+            subject='Book Loaned Successfully',
+            message=f'Hello {loan.member.user.username},\n\nYou have successfully loaned "{book_title}".\nPlease return it by the due date.',
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[member_email],
+            fail_silently=False,
+        )
+    except Loan.DoesNotExist:
+        pass
+@shared_task
+def check_overdue_loans():
+    overdue_loans = Loan.objects.filter(is_returned=False, due_date__lte=timezone.now())
+    for loan in overdue_loans:
+        send_mail(
+            subject='Overdue book',
+            message=f'Hello {loan.member.user.username}\n Your book is overdue.',
+            from_email = settings.DEFAULT_FROM_EMAIL,
+            recipient_list = [member_email],
+            fail_silently = False,
+        )
